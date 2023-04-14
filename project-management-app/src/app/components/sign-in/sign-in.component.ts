914a4be3ec0caf6fa8730aba5e1292e0aca56ec9
Marinayurkevich <roxy-lo@inbox.ru>
2023-04-14 13:20:08 +0300

    feat: implement sign in form

--- project-management-app/src/app/components/sign-in/sign-in.component.ts
+++ project-management-app/src/app/components/sign-in/sign-in.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from '../../../assets/classes';
+import { ToastrService } from 'ngx-toastr';
+import { NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { HttpService } from '../../http.service';
 
 
 @Component({
@@ -10,9 +15,33 @@ import { Router } from '@angular/router';
 export class SignInComponent {
   constructor(
     private router: Router,
-  ) { }
+    private toastr: ToastrService,
+    private http: HttpClient,
+    private httpService: HttpService) { }
 
-  async goToRegistration () {
+
+  user: User = new User("", "", "");
+
+  async goToRegistration() {
     await this.router.navigate(['/register'])
+  }
+
+
+  submit(form: NgForm) {
+
+
+    this.httpService.postLogin(form)
+      .subscribe({
+        next: (data: any) => { console.log(data.token); this.toastr.success("Successeful login"); },
+        error: (error: any) => {
+          //console.log(error);
+          if (error.status === 401 || error.status === 400) {
+            console.log(error.statusCode);
+
+          this.toastr.error(`Error ${error.status}. Authorization error. Try again, please!`);
+          }
+        },
+
+      });
   }
 }
--- project-management-app/src/app/http.service.ts
+++ project-management-app/src/app/http.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core'
-import { HttpClient, HttpParams } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http'
 import { NgForm } from '@angular/forms';
 
 @Injectable()
@@ -12,6 +12,12 @@ export class HttpService {
   postData(form: NgForm) {
     this.ending = "auth/signup";
     const body = { name: form.value.name, login: form.value.login, password: form.value.password };
+    return this.http.post(this.URL + this.ending, body);
+  }
+
+  postLogin(form: NgForm) {
+    this.ending = "auth/signin";
+    const body = { login: form.value.login, password: form.value.password };
     return this.http.post(this.URL + this.ending, body);
   }
 
